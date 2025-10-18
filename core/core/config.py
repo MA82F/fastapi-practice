9c711aed702084120b0e43881c523bc89990d71b
MohammadAmin Fahimi <fahimi13823182@gmail.com>
2025-10-18 23:13:52 +0330

    assigning default values(for action works)

--- core/core/config.py
+++ core/core/config.py
@@ -2,9 +2,9 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
-    SQLALCHEMY_DATABASE_URL: str
+    SQLALCHEMY_DATABASE_URL: str = "sqlite:///:memory:"
     JWT_SECRET_KEY: str = "this is a secret key:)))))/"
-    REDIS_URL: str
+    REDIS_URL: str = "redis://redis:6379"
     SENTRY_DSN: str = "https://92b4489ec7b5748a613999eab821ccd1@sentry.hamravesh.com/9174"
 
     model_config = SettingsConfigDict(env_file=".env")
